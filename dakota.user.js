6bbcc03f68da4796af9d6f8d5e3f3e71614dedd7
James Kastrantas <james@whatshappen.in>
2012-07-15 22:48:48 -0500

    Change @namespace and close Metadata block

--- dakota.user.js
+++ dakota.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name  Dakota
-// @namespace http://www.thefive22.com/
+// @namespace http://www.whatshappen.in/
 // @description Forces the Mr. Saturn typeface for YouTube comments.
 // @match http://www.youtube.com/*
+// ==/UserScript==
 
 var stylesheet = document.createElement('style');
 stylesheet.innerHTML = 'div .comment-text { font-family: earthbound; }';
